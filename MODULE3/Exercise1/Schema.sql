e26ecc8aa352af9e5874c20a83bc7085ca6d1baf
Sharika P K <sharikavipanchika@gmail.com>
2026-06-25 18:54:24 +0530

    Update and rename Data.sql to Schema.sql

---
+++ MODULE3/Exercise1/Schema.sql
@@ -0,0 +1,165 @@
+CREATE TABLE Customers (
+
+CustomerID NUMBER PRIMARY KEY,
+
+Name VARCHAR2(100),
+
+DOB DATE,
+
+Balance NUMBER,
+
+LastModified DATE
+
+);
+
+CREATE TABLE Accounts (
+
+AccountID NUMBER PRIMARY KEY,
+
+CustomerID NUMBER,
+
+AccountType VARCHAR2(20),
+
+Balance NUMBER,
+
+LastModified DATE,
+
+FOREIGN KEY (CustomerID)
+REFERENCES Customers(CustomerID)
+
+);
+
+CREATE TABLE Transactions (
+
+TransactionID NUMBER PRIMARY KEY,
+
+AccountID NUMBER,
+
+TransactionDate DATE,
+
+Amount NUMBER,
+
+TransactionType VARCHAR2(10),
+
+FOREIGN KEY(AccountID)
+REFERENCES Accounts(AccountID)
+
+);
+
+CREATE TABLE Loans (
+
+LoanID NUMBER PRIMARY KEY,
+
+CustomerID NUMBER,
+
+LoanAmount NUMBER,
+
+InterestRate NUMBER,
+
+StartDate DATE,
+
+EndDate DATE,
+
+FOREIGN KEY(CustomerID)
+REFERENCES Customers(CustomerID)
+
+);
+
+CREATE TABLE Employees (
+
+EmployeeID NUMBER PRIMARY KEY,
+
+Name VARCHAR2(100),
+
+Position VARCHAR2(50),
+
+Salary NUMBER,
+
+Department VARCHAR2(50),
+
+HireDate DATE
+
+);
+
+ALTER TABLE Customers
+ADD IsVIP VARCHAR2(5);
+
+INSERT INTO Customers
+VALUES
+(1,'John Doe',TO_DATE('1985-05-15','YYYY-MM-DD'),1000,SYSDATE,'FALSE');
+
+INSERT INTO Customers
+VALUES
+(2,'Jane Smith',TO_DATE('1990-07-20','YYYY-MM-DD'),1500,SYSDATE,'FALSE');
+
+INSERT INTO Customers
+VALUES
+(3,'Robert King',TO_DATE('1955-02-10','YYYY-MM-DD'),20000,SYSDATE,'FALSE');
+
+INSERT INTO Customers
+VALUES
+(4,'Mary James',TO_DATE('1958-08-25','YYYY-MM-DD'),15000,SYSDATE,'FALSE');
+
+COMMIT;
+
+INSERT INTO Accounts
+VALUES
+(1,1,'Savings',1000,SYSDATE);
+
+INSERT INTO Accounts
+VALUES
+(2,2,'Checking',1500,SYSDATE);
+
+INSERT INTO Accounts
+VALUES
+(3,3,'Savings',20000,SYSDATE);
+
+INSERT INTO Accounts
+VALUES
+(4,4,'Savings',15000,SYSDATE);
+
+COMMIT;
+
+INSERT INTO Transactions
+VALUES
+(1,1,SYSDATE,200,'Deposit');
+
+INSERT INTO Transactions
+VALUES
+(2,2,SYSDATE,300,'Withdrawal');
+
+COMMIT;
+
+INSERT INTO Loans
+VALUES
+(1,1,5000,5,SYSDATE,ADD_MONTHS(SYSDATE,60));
+
+INSERT INTO Loans
+VALUES
+(2,2,8000,6,SYSDATE,ADD_MONTHS(SYSDATE,24));
+
+INSERT INTO Loans
+VALUES
+(3,3,10000,7,SYSDATE,SYSDATE+20);
+
+INSERT INTO Loans
+VALUES
+(4,4,12000,8,SYSDATE,SYSDATE+15);
+
+COMMIT;
+
+INSERT INTO Employees
+VALUES
+(1,'Alice Johnson','Manager',70000,'HR',
+TO_DATE('2015-06-15','YYYY-MM-DD'));
+
+INSERT INTO Employees
+VALUES
+(2,'Bob Brown','Developer',60000,'IT',
+TO_DATE('2017-03-20','YYYY-MM-DD'));
+
+COMMIT;
+
+SELECT * FROM Customers;
+SELECT * FROM Accounts;
+SELECT * FROM Loans;

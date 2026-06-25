476f3c988160896b7dc2de4e196b07a5edf72e89
Sharika P K <sharikavipanchika@gmail.com>
2026-06-25 15:12:47 +0530

    Create Scenario1.sql

---
+++ MODULE3/Exercise1/Scenario1.sql
@@ -0,0 +1,21 @@
+BEGIN
+    FOR cust IN (
+        SELECT CustomerID
+        FROM Customers
+        WHERE Age > 60
+    )
+    LOOP
+
+        UPDATE Loans
+        SET InterestRate = InterestRate - 1
+        WHERE CustomerID = cust.CustomerID;
+
+    END LOOP;
+
+    COMMIT;
+
+    DBMS_OUTPUT.PUT_LINE('Interest rates updated successfully.');
+
+END;
+/
+SELECT * FROM Loans;

25e5ebe1b6c8835807974cd114efbfa52e11ecae
Sharika P K <sharikavipanchika@gmail.com>
2026-06-25 15:20:57 +0530

    Create Scenario3.sql

---
+++ MODULE3/Exercise1/Scenario3.sql
@@ -0,0 +1,34 @@
+SET SERVEROUTPUT ON;
+
+BEGIN
+
+    FOR loan_rec IN (
+
+        SELECT c.CustomerName,
+               l.LoanID,
+               l.DueDate
+
+        FROM Customers c
+        JOIN Loans l
+
+        ON c.CustomerID=l.CustomerID
+
+        WHERE l.DueDate BETWEEN SYSDATE AND SYSDATE+30
+
+    )
+
+    LOOP
+
+        DBMS_OUTPUT.PUT_LINE(
+            'Reminder: Dear '
+            ||loan_rec.CustomerName||
+            ', Loan ID '
+            ||loan_rec.LoanID||
+            ' is due on '
+            ||TO_CHAR(loan_rec.DueDate,'DD-MON-YYYY')
+        );
+
+    END LOOP;
+
+END;
+/

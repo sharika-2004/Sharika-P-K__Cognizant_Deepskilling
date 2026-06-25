77cc9ba08621b1af5a841c5ec905380820fac56c
Sharika P K <sharikavipanchika@gmail.com>
2026-06-25 18:55:04 +0530

    Update Scenario1.sql

--- MODULE3/Exercise1/Scenario1.sql
+++ MODULE3/Exercise1/Scenario1.sql
@@ -1,21 +1,30 @@
 BEGIN
-    FOR cust IN (
-        SELECT CustomerID
-        FROM Customers
-        WHERE Age > 60
-    )
-    LOOP
 
-        UPDATE Loans
-        SET InterestRate = InterestRate - 1
-        WHERE CustomerID = cust.CustomerID;
+FOR cust IN(
 
-    END LOOP;
+SELECT CustomerID
 
-    COMMIT;
+FROM Customers
 
-    DBMS_OUTPUT.PUT_LINE('Interest rates updated successfully.');
+WHERE FLOOR(MONTHS_BETWEEN(SYSDATE,DOB)/12)>60
+
+)
+
+LOOP
+
+UPDATE Loans
+
+SET InterestRate=InterestRate-1
+
+WHERE CustomerID=cust.CustomerID;
+
+END LOOP;
+
+COMMIT;
+
+DBMS_OUTPUT.PUT_LINE('Interest Updated');
 
 END;
 /
+
 SELECT * FROM Loans;

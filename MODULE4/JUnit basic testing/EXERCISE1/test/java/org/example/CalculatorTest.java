35f9918cfe606182b52712649b2442b5b4a4b408
Sharika P K <sharikavipanchika@gmail.com>
2026-06-29 13:12:59 +0530

    Added CalculatorTest.java

---
+++ MODULE4/JUnit basic testing/EXERCISE1/test/java/org/example/CalculatorTest.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAddition() {
+
+        Calculator calculator = new Calculator();
+
+        int result = calculator.add(10, 20);
+
+        assertEquals(30, result);
+
+    }
+
+}
+

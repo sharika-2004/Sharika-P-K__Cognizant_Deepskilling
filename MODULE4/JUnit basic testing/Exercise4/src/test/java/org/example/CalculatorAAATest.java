4140485ad8f4f048684141a43698941cb0f1fea7
Sharika P K <sharikavipanchika@gmail.com>
2026-06-30 11:56:10 +0530

    Add files via upload

---
+++ MODULE4/JUnit basic testing/Exercise4/src/test/java/org/example/CalculatorAAATest.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorAAATest {
+
+    private Calculator calculator;
+
+    @Before
+    public void setUp() {
+        System.out.println("Setting up Calculator...");
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void testAddition() {
+
+        // Arrange
+        int a = 10;
+        int b = 20;
+
+        // Act
+        int result = calculator.add(a, b);
+
+        // Assert
+        assertEquals(30, result);
+    }
+
+    @After
+    public void tearDown() {
+        System.out.println("Cleaning up...");
+        calculator = null;
+    }
+}

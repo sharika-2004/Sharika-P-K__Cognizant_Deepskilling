b25b7dd5329e54079b809409d1095e6c68e9e2d4
Sharika P K <sharikavipanchika@gmail.com>
2026-07-02 22:16:54 +0530

    Add files via upload

---
+++ Module7/REST_Get_country_based_on_country_code/spring_learn/service/CountryService.java
@@ -0,0 +1,41 @@
+package com.cognizant.spring_learn.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Service;
+
+import com.cognizant.spring_learn.Country;
+
+@Service
+public class CountryService {
+
+    public Country getCountry(String code) {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("country.xml");
+
+        List<Country> countries = new ArrayList<>();
+
+        countries.add(context.getBean("country1", Country.class));
+        countries.add(context.getBean("country2", Country.class));
+        countries.add(context.getBean("country3", Country.class));
+        countries.add(context.getBean("country4", Country.class));
+
+        for (Country country : countries) {
+
+            if (country.getCode().equalsIgnoreCase(code)) {
+
+                ((ClassPathXmlApplicationContext) context).close();
+
+                return country;
+            }
+        }
+
+        ((ClassPathXmlApplicationContext) context).close();
+
+        return null;
+    }
+}
